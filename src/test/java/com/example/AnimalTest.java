c98d90b4aa352f62ebc4984a401e977849c0d264
person4eg <person4eg@bk.ru>
2021-11-02 19:35:08 +0400

    isolate Lion from Feline improve AnimalTest and LionTest with Enclosed

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -1,16 +1,61 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Enclosed.class)
 public class AnimalTest {
 
-    @Test
-    public void getFamilyTest(){
-        Animal animal = new Animal();
-        String excepted = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
-        String actual = animal.getFamily();
-        assertEquals(excepted, actual);
+    @RunWith(Parameterized.class)
+    public static class AnimalParametrizedTests {
+
+        private final String animalKind;
+        private final List<String> food;
+        private final Animal animal;
+
+        public AnimalParametrizedTests(String animalKind, List<String> food) {
+            this.animalKind = animalKind;
+            this.food = food;
+            this.animal = new Animal();
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] getAnimalTypeData() {
+            return new Object[][]{
+                    {"Травоядное", Arrays.asList("Трава", "Различные растения")},
+                    {"Хищник", Arrays.asList("Животные", "Птицы", "Рыба")},
+                    {"Млекопитающие", List.of("Неизвестный вид животного, используйте значение Травоядное или Хищник")}
+            };
+        }
+
+        @Test
+        public void getFoodTest() {
+            List<String> excepted = food;
+            List<String> actual;
+            try {
+                actual = animal.getFood(animalKind);
+            } catch (Exception e) {
+                actual = List.of(e.getMessage());
+            }
+            assertEquals(excepted, actual);
+        }
+    }
+
+    public static class AnimalSingleTests{
+
+        @Test
+        public void getFamilyTest(){
+            Animal animal = new Animal();
+            String excepted = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+            String actual = animal.getFamily();
+            assertEquals(excepted, actual);
+        }
     }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mock;
@@ -13,32 +14,79 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Enclosed.class)
 public class LionTest {
 
-    @Mock
-    private Feline feline;
+    @RunWith(Parameterized.class)
+    public static class LionParametrizedTests {
 
-    @Before
-    public void init() {
-        MockitoAnnotations.initMocks(this);
+        @Mock
+        private Feline feline;
+
+        private final String sex;
+        private final String hasMane;
+
+        public LionParametrizedTests(String sex, String hasMane) {
+            this.sex = sex;
+            this.hasMane = hasMane;
+        }
+
+        @Before
+        public void init() {
+            MockitoAnnotations.initMocks(this);
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] getLionData() {
+            return new Object[][] {
+                    {"Самец", "true"},
+                    {"Самка", "false"},
+                    {"Самей", "Используйте допустимые значения пола животного - самей или самка"}
+            };
+        }
+
+        @Test
+        public void doesHaveManeTest() {
+            try {
+                Lion lion = new Lion(feline, sex);
+                boolean excepted = Boolean.parseBoolean(hasMane);
+                boolean actual = lion.doesHaveMane();
+                assertEquals(excepted, actual);
+            } catch (Exception e) {
+                String excepted = hasMane;
+                String actual = e.getMessage();
+                assertEquals(excepted, actual);
+            }
+        }
     }
 
-    @Test
-    public void getKittensTest() throws Exception {
-        Mockito.when(feline.getKittens()).thenReturn(1);
-        Lion lion = new Lion("Самец");
-        lion.feline = feline;
-        int excepted = 1;
-        int actual = lion.getKittens();
-        assertEquals(excepted, actual);
-    }
-    @Test
-    public void getFoodTest() throws Exception {
-        Mockito.when(feline.getFood(Mockito.anyString())).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
-        Lion lion = new Lion("Самка");
-        lion.feline = feline;
-        List<String> excepted = Arrays.asList("Животные", "Птицы", "Рыба");
-        List<String> actual = lion.getFood();
-        assertEquals(excepted, actual);
+    public static class LionSingleTests {
+
+        @Mock
+        private Feline feline;
+
+        @Before
+        public void init() {
+            MockitoAnnotations.initMocks(this);
+        }
+
+        @Test
+        public void getKittensTest() throws Exception {
+            Mockito.when(feline.getKittens()).thenReturn(1);
+            Lion lion = new Lion(feline, "Самец");
+            lion.feline = feline;
+            int excepted = 1;
+            int actual = lion.getKittens();
+            assertEquals(excepted, actual);
+        }
+        @Test
+        public void getFoodTest() throws Exception {
+            Mockito.when(feline.getFood(Mockito.anyString())).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
+            Lion lion = new Lion(feline,"Самка");
+            lion.feline = feline;
+            List<String> excepted = Arrays.asList("Животные", "Птицы", "Рыба");
+            List<String> actual = lion.getFood();
+            assertEquals(excepted, actual);
+        }
     }
 }
